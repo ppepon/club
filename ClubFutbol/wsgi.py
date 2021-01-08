e459e4423a3047cf6fabcccc85cbb8c983c72918
ppepon <ppepon@gmail.com>
2021-01-08 14:04:32 +0100

    5

--- ClubFutbol/wsgi.py
+++ ClubFutbol/wsgi.py
@@ -13,11 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ClubFutbol.settings')
 
-application = get_wsgi_application()
-'''from dj_static import Cling
+#application = get_wsgi_application()
+from dj_static import Cling
 
-application = Cling(get_wsgi_application())'''
-
-from whitenoise.django import DjangoWhiteNoise  
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
 
