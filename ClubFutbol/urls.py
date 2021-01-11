138f04a0127fbb347574262dfa10da4ffdc4b456
ppepon <ppepon@gmail.com>
2021-01-11 10:29:22 +0100

    nueva subida

--- ClubFutbol/urls.py
+++ ClubFutbol/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] 
-urlpatterns =urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += patterns("",
+(r"^static/(?P<path>.*)$","django.views.static.serve",{"document_root":settings.STATIC_ROOT,"show_indexes": False}),
+)
--- ClubFutbol/wsgi.py
+++ ClubFutbol/wsgi.py
@@ -9,11 +9,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
-#from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ClubFutbol.settings')
 
-#application = get_wsgi_application()
+application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
