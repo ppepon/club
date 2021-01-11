446295f1ae9f838fa618d822dcc371a0bc469c5d
ppepon <ppepon@gmail.com>
2021-01-11 10:03:33 +0100

    nueva subida2

--- ClubFutbol/urls.py
+++ ClubFutbol/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+urlpatterns =urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
